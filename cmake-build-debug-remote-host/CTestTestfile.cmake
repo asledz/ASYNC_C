a235d417c05f8713383beb7d683ed7227326208f
asledz <anita.gaja.sledz@gmail.com>
2020-01-04 15:36:33 +0100

    Usunięte foldery niepotrzebne

--- cmake-build-debug-remote-host/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.7SuB7nBQlt
-# Build directory: /tmp/tmp.7SuB7nBQlt/cmake-build-debug-remote-host
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- cmake-build-debug-remote-host/test/CMakeFiles/test_await.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_await.
-# This may be replaced when dependencies are built.
